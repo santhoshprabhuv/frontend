c921e64cd050711ff0a9685a8dd9c36d78a8765f
santhoshprabhuV2003 <santhoshprabhuofficial@gmail.com>
2024-10-16 18:02:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+import TasksList from './components/TaskList';
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTaskTitle, setNewTaskTitle] = useState('');
+
+  const getTasks = () => {
+    fetch('http://localhost:5000/api/tasks')
+      .then(res => res.json())
+      .then(setTasks);
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const clickAddTask = event => {
+    event.preventDefault();
+
+    fetch('http://localhost:5000/api/tasks', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: newTaskTitle }),
+    }).then(() => {
+      setNewTaskTitle('');
+      getTasks();
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Tasks</h1>
+
+      <TasksList tasks={tasks} updateTasks={getTasks} />
+
+      <form onSubmit={clickAddTask}>
+        <input
+          type="text"
+          size="30"
+          placeholder="New Task"
+          value={newTaskTitle}
+          onChange={event => setNewTaskTitle(event.target.value)}
+        />
+        <input className="btn-primary" type="submit" value="Add" />
+      </form>
     </div>
   );
-}
+};
 
 export default App;
